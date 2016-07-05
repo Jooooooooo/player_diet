2df5cdaad91ff121fe6bb72ae9cac9fdac7bafa4
wngus <beloved731@naver.com>
2016-07-05 16:47:57 +0900

    arm64 없앰.

--- ijkplayer-arm64/src/main/obj/local/arm64-v8a/objs/android-ndk-profiler/prof.o.d
+++
@@ -1,8 +0,0 @@
-/root/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/obj/local/arm64-v8a/objs/android-ndk-profiler/prof.o: \
- /root/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.c \
- /root/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.h \
- /root/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/log.h
-
-/root/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.h:
-
-/root/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/log.h:
